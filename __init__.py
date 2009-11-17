b01c6c8eb5764350580e18268a7befd2b71c811a
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-11-17 14:45:47 +0000

    Upgraded for Plone 3.x compatibility

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
+from Products.CMFCore.DirectoryView import registerDirectory
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.FacultyStaffDirectory.extenderInstallation import installExtenderGloballyIfLocallyIsNotSupported
+from Products.GenericSetup import EXTENSION, profile_registry
 
 from Products.huckPersonExtender.person import additionalInfoExtender
 from Products.huckPersonExtender.person import bldgDirExtender
@@ -7,3 +10,31 @@ from Products.huckPersonExtender.person import modifyHuckFields
 installExtenderGloballyIfLocallyIsNotSupported(additionalInfoExtender, 'huckPersonExtender.additionalInfoExtender')
 installExtenderGloballyIfLocallyIsNotSupported(bldgDirExtender, 'huckPersonExtender.bldgDirExtender')
 installExtenderGloballyIfLocallyIsNotSupported(modifyHuckFields, 'huckPersonExtender.modifyHuckFields')
+
+registerDirectory('skins', globals())
+
+def initialize(context):
+    profile_registry.registerProfile(
+        "default",
+        "huckPersonExtender",
+        "Customize the Faculty/Staff Directory's Person type for the Huck Institutes.",
+        "profiles/default",
+        product="Products.huckPersonExtender",
+        profile_type=EXTENSION,
+        for_=IPloneSiteRoot)
+    profile_registry.registerProfile(
+        "uninstall",
+        "huckPersonExtender Uninstall",
+        "Removes the changes to the Faculty/Staff Directory's Person type to customize it for the Huck Institutes.",
+        "profiles/uninstall",
+        product="Products.huckPersonExtender",
+        profile_type=EXTENSION,
+        for_=IPloneSiteRoot)
+
+
+
+
+
+
+
+
