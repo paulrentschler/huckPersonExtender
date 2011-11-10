7a0aa253aac219a5a750ff600fd0d960370a1dd1
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2011-11-10 19:54:51 +0000

    Added termination details field

--- huckInstitutes/Huck/huckPersonExtender/trunk/person.py
+++ huckInstitutes/Huck/huckPersonExtender/trunk/person.py
@@ -14,6 +14,8 @@ from Products.FacultyStaffDirectory.interfaces.person import IPerson
 # Any field you tack on must have ExtensionField as its first subclass:
 class _StringExtensionField(ExtensionField, StringField):
     pass
+class _TextExtensionField(ExtensionField, TextField):
+    pass
 class _BooleanExtensionField(ExtensionField, BooleanField):
     pass
 class _RelationExtensionField(ExtensionField, RelationField):
@@ -112,6 +114,21 @@ class addHuckFields(object):
                 allowed_types=('FSDPerson'),
                 multiValued=True,
                 relationship='people_advisors'
+            ),
+            
+            _TextExtensionField('terminationDetails',
+                required=False,
+                searchable=False,
+                schemata="Administrative",
+                validators=('isTidyHtmlWithCleanup',),
+                default_output_type='text/x-html-safe',
+                allowable_content_types=ALLOWABLE_CONTENT_TYPES,
+                user_property='description'
+                widget=RichWidget(
+                    label=_(u"FacultyStaffDirectory_label_termination_details", default=u"Termination details"),
+                    description=_(u"FacultyStaffDirectory_description_termination_details", default=u"Message displayed to site visitors when the person's termination date has passed. Can be used to provide forwarding information or a link to someone who has taken over their responsibilities."),
+                    i18n_domain='FacultyStaffDirectory',
+                ),
             ),
             
         ]
@@ -281,10 +298,6 @@ class modifyHuckFields(object):
         tmp_field.widget.description = "When this date is reached, the person will be marked as having left the Huck Institutes."
         schema['expirationDate'] = tmp_field
         
-        tmp_field = schema['terminationDetails'].copy()
-        tmp_field.schemata = "Administrative"
-        schema['terminationDetails'] = tmp_field
-
         # move some typically hidden fields to the right schemata
         for fieldName in ['title', 'password', 'confirmPassword']:
             tmp_field = schema[fieldName].copy()
